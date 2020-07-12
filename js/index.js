be1d481b3965094df4eeaa691245c826c70febd2
Joca <ilicjovan89@gmail.com>
2020-07-12 15:05:52 +0200

    Update portfolio

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
-	setTimeout ( function () {
+$(document).ready(function () {
+	setTimeout(function () {
 		var kinks = $("#kinks div").height();
 		var features = $("#features div").height();
 		if (kinks < features)
 			$("#kinks div").height(features);
 		else if (kinks > features)
 			$("#features div").height(kinks);
-		
-		var photoHeight = $("#photo").height();
-		var photodivHeight = $("#photodiv").height();
-		var photoMargin = photodivHeight/2 - photoHeight/2
-		$("#photo").css("margin-top", photoMargin);
 	}, 10);
-	//test webhook
+
+	var flag = true;
+	setInterval(function() {
+		$("#typer").fadeTo("fast", flag);
+		flag = !flag;
+	}, 500);
 });
